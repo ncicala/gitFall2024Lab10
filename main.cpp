bdaa519b6cb1a957b7857b2cc802210513bdf5ef
AmberRMartin <armartin3@nic.edu>
2024-10-22 13:27:55 -0700

    display name function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using std::cout;
 
+void displayName(std::string stuff);
+
 int main()
 {
     cout<< "Hello world!\n";
+    std::string name = "Amber";
+    displayName(name);
     return 0;
+}
+
+void displayName(std::string stuff)
+{
+    cout<< "hey guys, it's " <<stuff<<std::endl;
 }
